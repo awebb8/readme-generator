70269d4ff9bd289f406d504315ad3a6d4a34bbe4
Alkawe <alixkwebb@gmail.com>
2020-09-22 21:29:38 -0400

    added inputs to the questions array

--- index.js
+++ index.js
@@ -28,6 +28,31 @@ const questions = [
       type: "input",
       message: "How is the site or file used?",
       name: "usage",
+    },
+    {
+      type: "input",
+      message: "What is the associated license?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "How can others contribute?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "What tests have been completed?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What tests have been completed?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Where can people direct questions to?",
+      name: "questions"
     }
 ];
 
@@ -54,3 +79,9 @@ writeToFile(response);
 
 // function call to initialize program
 // init();
+
+
+// WRITE function for download of inquirer??
+    // So that anyone can pull this from github and use it
+
+// Does there need to be an option for if you don't want to input data for any of these sections?
